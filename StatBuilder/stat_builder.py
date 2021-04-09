13bce5e7f01f3467e01b9046430d90529d60a7fc
jamaica <myunsoo@gmail.com>
2021-04-09 22:22:46 +0900

    think it's working

--- StatBuilder/stat_builder.py
+++ StatBuilder/stat_builder.py
@@ -1,12 +1,50 @@
 import csv
 import yaml
+import os
 
 
 def main():
-    with open(".config.yaml") as myconfig_file:
-        config = yaml.load(myconfig_file)
 
-    print("hello world")
+    statdict = dict()
+
+    with open("config.yaml") as myconfig_file:
+        config = yaml.safe_load(myconfig_file)
+
+    for dirpath, dnames, fnames in os.walk(config["CSV_FOLDER"]):
+        for fn in fnames:
+            if fn.endswith(".csv"):
+                # csv loader too much, do it later
+                fp = open(os.path.join(dirpath, fn), "r")
+                for line in fp.readlines():
+                    items = line.replace("\n","").split(",")
+                    # print(items)
+                    pn = items[0]
+                    if pn not in statdict:
+                        statdict[pn] = dict()
+
+                    for i in range(1, len(items)):
+                        colname = config["COLNAMES"][i]
+                        if colname not in statdict[pn]:
+                            statdict[pn][colname] = 0
+
+                        statdict[pn][colname] += int(items[i])
+
+                fp.close()
+            # elif f.endswith(".xc"):
+            #     xc(os.path.join(dirpath, f))
+            # import pprint
+            # pprint.pprint(statdict)
+
+    # write
+    fw = open(config["OUTPUT_FOLDER"] + os.sep + "output.csv", "w")
+    for pn in statdict:
+        fw.write(pn)
+        for i in range(1, len(config["COLNAMES"])):
+            colname = colname = config["COLNAMES"][i]
+            fw.write(",")
+            fw.write(str(statdict[pn][colname]))
+        fw.write("\n")
+    fw.close()
 
 
 
